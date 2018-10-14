01b7512b2b53179e65a47c3ef4b7c873b9c68b05
xieqiang <chanxueer@sohu.com>
2018-10-14 11:06:44 +0800

    时间查询”

--- projects/admin/controllers/UserController.php
+++ projects/admin/controllers/UserController.php
@@ -27,4 +27,20 @@ class UserController extends BaseController
 
         return ArrayHelper::merge($parentBehaviors, $behaviors);
     }
+
+    public function actionIndex()
+    {
+        $find = BUser::find();
+        $pageSize = $this->pInt('pageSize');
+        $page = $this->pInt('page');
+        $searchName = $this->pString('searchName');
+        
+        if ($searchName != '') {
+            $find->andWhere(['like','username',$searchName]);
+        }
+        $str_time = $this->pString('str_time');
+        $end_time = $this->pString('end_time');
+        $count = $find->count();
+        $list = $find->page($page, $pageSize)->asArray()->all();
+    }
 }

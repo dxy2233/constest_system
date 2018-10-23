962651560d3b3993c23cff54e05a80b27aa1cecd
PHP-XLJ-COMPANY <737043980@qq.com>
2018-10-23 16:12:45 +0800

    更新前端以及接口时间显示

--- projects/app/controllers/NoticeController.php
+++ projects/app/controllers/NoticeController.php
@@ -56,6 +56,7 @@ class NoticeController extends BaseController
         $data['type_str'] = BNotice::getType($notice->type);
         // 模型中格式化时间
         $data['create_Time'] = $notice->createTimeText;
+        $data['update_Time'] = $notice->updateTimeText;
         $data['start_Time'] = $notice->startTimeText;
         $data['end_time'] = $notice->endTimeText;
         unset($data['sort']);

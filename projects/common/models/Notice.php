65c21f9c6c8fd77212ddb49cb21f569282a45d66
PHP-XLJ-COMPANY <737043980@qq.com>
2018-10-23 13:15:52 +0800

    更新后台公告编辑，url判断

--- projects/common/models/Notice.php
+++ projects/common/models/Notice.php
@@ -39,8 +39,9 @@ class Notice extends \common\dzbase\DzModel
         return [
             [['title', 'type'], 'required'],
             [['detail'], 'string'],
-            [['type', 'sort', 'status',  'click', 'create_time', 'update_time'], 'integer'],
+            [['type', 'sort', 'status', 'click', 'create_time', 'update_time'], 'integer'],
             [['title', 'desc', 'url', 'image'], 'string', 'max' => 255],
+            ['url', 'url', 'defaultScheme' => 'http'],
         ];
     }
 

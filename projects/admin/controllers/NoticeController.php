5c6eda9a1c2b7a7775b54cd5f95835bdd9890bdd
xieqiang <chanxueer@sohu.com>
2018-10-23 09:23:38 +0800

    公告状态筛选

--- projects/admin/controllers/NoticeController.php
+++ projects/admin/controllers/NoticeController.php
@@ -46,7 +46,7 @@ class NoticeController extends BaseController
         $count = $find->count();
         $page = $this->pInt('page', 1);
         $find->page($page);
-        echo $find->createCommand()->getRawSql();
+        //echo $find->createCommand()->getRawSql();
         $data = $find->asArray()->all();
         foreach ($data as &$v) {
             $v['create_time'] = date('Y-m-d H:i:s', $v['create_time']);

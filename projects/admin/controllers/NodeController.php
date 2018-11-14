ffd059d28ff96dc267d227d746fce80a6bd46bbe
xieqiang <chanxueer@sohu.com>
2018-11-14 13:52:38 +0800

    修改bug

--- projects/admin/controllers/NodeController.php
+++ projects/admin/controllers/NodeController.php
@@ -198,6 +198,7 @@ class NodeController extends BaseController
         $currencyDetail->remark = '申请节点奖励';
         $currencyDetail->user_id = $data->user_id;
         $currencyDetail->relate_table = 'node';
+        $currencyDetail->type = BUserCurrencyDetail::$TYPE_REWARD;
         $currencyDetail->relate_id = $data->id;
         $currencyDetail->amount = $node_type->gdt_reward;
         if (!$currencyDetail->save()) {

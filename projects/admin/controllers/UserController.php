c47ebb9b480253f6710f1a1ec11c3e41128897c9
xieqiang <chanxueer@sohu.com>
2018-11-14 09:58:46 +0800

    修改bug

--- projects/admin/controllers/UserController.php
+++ projects/admin/controllers/UserController.php
@@ -255,7 +255,7 @@ class UserController extends BaseController
     }
 
 
-    // 获取用户实名信息
+    // 获取用户实名信息 只获取已通过的
     public function actionGetUserIdentify()
     {
         $userId = $this->pInt('userId');
--- projects/common/models/UserOther.php
+++ projects/common/models/UserOther.php
@@ -40,7 +40,7 @@ class UserOther extends \common\dzbase\DzModel
     public function rules()
     {
         return [
-            [['user_id', 'area_province_id', 'area_city_id', 'zip_code', 'create_time', 'update_time'], 'integer'],
+            [['user_id', 'area_province_id', 'area_city_id', 'create_time', 'update_time'], 'integer'],
             [['consignee'], 'string', 'max' => 64],
             [['consignee_mobile', 'recommend_mobile', 'recommend_name'], 'string', 'max' => 32],
             [['address', 'weixin'], 'string', 'max' => 128],

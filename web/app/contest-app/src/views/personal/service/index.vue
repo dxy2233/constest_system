65eca2c6c6fcafec64a19619bc2c7fa6c419e9e6
Jadepipe <1065682096@qq.com>
2018-11-16 13:21:31 +0800

    'add-online-service'

---
+++ web/app/contest-app/src/views/personal/service/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <slide>
+    <div class="service">
+      <app-header>
+        在线客服
+      </app-header>
+      <iframe id="service-content" src="https://im.7x24cc.com/phone_webChat.html?accountId=N000000010737&chatId=adf12f09-7c82-4519-8042-934394dc2651" frameborder="0"></iframe>
+    </div>
+  </slide>
+</template>
+
+<script>
+  import slide from 'components/slide/index'
+  import http from 'js/http'
+
+  export default {
+    name: "index",
+    components: {
+      slide
+    },
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "~stylus/variable"
+  @import "~stylus/mixin"
+  .service
+    fixed-full-screen()
+    overflow auto
+    #service-content
+      margin-top 50px
+      width 100%
+      height calc(100% - 50px)
+
+</style>

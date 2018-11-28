e3e34663a7a8f32cfca5119970fe9ceeb9d265ba
PHP-XLJ-COMPANY <737043980@qq.com>
2018-11-28 11:06:48 +0800

    更新绑定推荐关系赠送投票券控制台方法

--- projects/console/controllers/IndexController.php
+++ projects/console/controllers/IndexController.php
@@ -8,9 +8,12 @@
 
 namespace console\controllers;
 
-use common\models\business\BSetting;
+use Yii;
 use common\services\JobService;
+use common\services\NodeService;
+use common\models\business\BNode;
 use common\services\UpdateService;
+use common\models\business\BSetting;
 
 class IndexController extends BaseController
 {
@@ -45,5 +48,36 @@ class IndexController extends BaseController
     public function actionCheckRecommend()
     {
         UpdateService::checkRecommend();
+    }
+
+    /**
+     * 已存在推荐关系赠送投票劵以及GDT
+     *
+     * @return void
+     */
+    public function actionResetVoucherGdt()
+    {
+        $date = date('Y-m-d H:i:s', time());
+        $begin =  $date . '重算推荐关系绑定赠送投票劵和GDT开始.';
+        echo $begin . PHP_EOL;
+        Yii::info($begin, 'VoucherGDT');
+        $nodeModel = BNode::find()->active()->orderBy(['user_id' => SORT_DESC])->all();
+        foreach ($nodeModel as $model) {
+            $giveInfo = '用户ID：'.$model->user_id;
+            $result = NodeService::checkVoucher($model->user_id);
+            $msg = $giveInfo.$result->msg;
+            if ($result->code) {
+                Yii::error($msg, 'VoucherGDT');
+                echo $msg . PHP_EOL;
+                continue;
+            }
+            Yii::info($msg, 'VoucherGDT');
+            echo $msg . PHP_EOL;
+        }
+        
+        $date = date('Y-m-d H:i:s', time());
+        $end = $date . '重算推荐关系绑定赠送投票劵和GDT结束';
+        Yii::info($end, 'VoucherGDT');
+        echo $end . PHP_EOL;
     }
 }

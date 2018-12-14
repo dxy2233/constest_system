b6918440b9dfd60285e7eea97b1cd0b36b5fa3cb
xieqiang <chanxueer@sohu.com>
2018-12-14 17:25:51 +0800

    错误判断

--- projects/admin/controllers/NodeController.php
+++ projects/admin/controllers/NodeController.php
@@ -282,6 +282,9 @@ class NodeController extends BaseController
         if (empty($data)) {
             return $this->respondJson(1, '不存在的节点');
         }
+        if($data->status == BNode::STATUS_ON){
+            return $this->respondJson(1, '错误的状态');
+        }
         $now_count = BNode::find()->where(['type_id' => $data->type_id, 'status' => BNode::STATUS_ON])->count();
         $node_type = BNodeType::find()->where(['id' => $data->type_id])->one();
         if ($now_count >= $node_type->max_candidate) {

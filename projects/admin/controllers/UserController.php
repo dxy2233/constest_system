8da6c82e28d54e74194034e67f43891a0582c27f
xieqiang <chanxueer@sohu.com>
2018-12-17 11:25:06 +0800

    推荐人bug

--- projects/admin/controllers/UserController.php
+++ projects/admin/controllers/UserController.php
@@ -867,6 +867,7 @@ class UserController extends BaseController
         foreach ($data as &$v) {
             $v['p_type_id'] = BNodeType::GetName($v['p_type_id']);
             $v['u_type_id'] = BNodeType::GetName($v['u_type_id']);
+            $v['create_time'] = date('Y-m-d H:i:s', $v['create_time']);
         }
         $return = [];
         $return['list'] = $data;
